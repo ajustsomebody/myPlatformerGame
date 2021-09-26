8e37dabb82fab252b99ebf98a568c818e22e40c5
logicminimal <logicminimal@gmail.com>
2021-09-26 23:10:29 +0300

    fixed line 29 and did more coding

--- core/src/main/kotlin/CorePlat/entity.kt
+++ core/src/main/kotlin/CorePlat/entity.kt
@@ -1,30 +1,29 @@
 package CorePlat
 
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Body
 import com.badlogic.gdx.physics.box2d.BodyDef
-import com.badlogic.gdx.physics.box2d.FixtureDef
-import sun.jvm.hotspot.oops.CellTypeState.ref
 import java.lang.Exception
 
 
-class entity() {
-    operator fun set(startPosition: Vector2, hp: Float, jumpMaxDuration: UInt): entity
+class entity(startPositionT: Vector2, hpT: Float, jumpMaxDurationT: Int) {
+
+    operator fun set(startPosition: Vector2, hp: Float, jumpMaxDuration: Int): entity
     { // Fully complete setter
         this.hp = hp
-        this.jumpMaxDuration = jumpMaxDuration
-        body.type = BodyDef.BodyType.DynamicBody;
-        body.position.set(startPosition)
+        this.jumpMaxDuration = jumpMaxDuration.toUInt()
+        bodyDefinition.type = BodyDef.BodyType.DynamicBody;
+        bodyDefinition.position.set(startPosition)
         return this
     }
 
     //Variables itself
-    var body: BodyDef = BodyDef();
+    var bodyDefinition: BodyDef = BodyDef();
     var hp: Float = 0f
-    var position: Vector2 get() {return body.position} set(newPos: Vector2) {body.position.set(newPos.x, newPos.y)}
-    var velocity = Float
+    var position: Vector2 get() {return body!!.position} set(newPos: Vector2) { body!!.position.set(newPos.x, newPos.y)}
+    var speed: Float = 10f
+    var runSpeed: Float = speed * 1.5f
     var isJumping: Boolean = false
     var isAfloat: Boolean = true
     var jumpPower: Float = 10f
@@ -33,43 +32,40 @@ class entity() {
     var attackDefence: Int = 0 // really should make this float but not now
     var attackResistance: Float = 1f
     var attackStrength: Float = 1f
-    val bodyInTheWorld: Body? = null // nulls mean that it will be initialised later
-    var fixture: FixtureDef? = null
-    var xDirection: Int = Input.Keys.RIGHT;
+    var body: Body? = null // nulls mean that it will be initialised later
+    var xDirection: Int = Input.Keys.RIGHT
+    var isRunning: Boolean = false
     var maxMachTime: UInt = 0.toUInt() // time (frames in this case) it takes to get from 0 velocity to the max
     var MachTimer: UInt =  0.toUInt()
-    var maxWalkSpeed: Float = 0f //incomplete
-    var maxRunSpeed: Float = 0f //incomplete
+    var maxWalkVelocity: Float = 0f //incomplete
+    var maxRunVelocity: Float = 0f //incomplete
 
-    fun jump() // this will make the player go up only when this is pressed
+
+    init {
+        this.hp = hpT
+        this.jumpMaxDuration = jumpMaxDurationT.toUInt()
+        bodyDefinition.type = BodyDef.BodyType.DynamicBody;
+        bodyDefinition.position.set(startPositionT)
+    }
+
+
+    fun jump() //
     {
         if(!(isJumping || isAfloat))
         {
-            if(jumpDuration > 0.toUInt()) //it can still be 0!
-            {
-                position.y+= jumpPower * jumpDuration.toFloat()/10f
-                jumpDuration--
-                //continue later
-            }
-            else
-            {
-                isJumping = false
-                jumpDuration = jumpMaxDuration
-            }
+            jumpDuration = jumpMaxDuration
+            isJumping == true
         }
-        else
+        /*if(canInfiniJump)
         {
-            if(!isAfloat)
-            {
-                isJumping = true
-                jump()
-            }
-        }
+            jumpDuration = jumpMaxDuration
+            isJumping == true
+        }*/
     }
     fun takeDamage(damageTaken: Float)
     {
         var damageToSubtract = (damageTaken * (1f-attackResistance)) - attackDefence
-        if(hp >= 0)
+        if(hp > 0)
         {
             if((hp - damageToSubtract) < 0f) damageToSubtract = hp // possible float decimal miscalculation
         }
@@ -82,17 +78,22 @@ class entity() {
         if(hp<=0) die()
 
     }
-    fun move(direction: Int)
+    fun move(direction: Int, run:Boolean)
     {
         if(!varOrCache.HorizontalDirectionArray.contains(direction))
         {
             throw Exception("Inputted an invalid direction (Horizontal needed)")
         }
-        else
-        {
-            position.x += 1f//change this later
-        }
+        else { // if you want to display velocity it will show up as minuses as well, if this velocity system
+            if (run == false) { // at its base doesn't cause any problems, just get the value without the
+                xDirection = direction // minus or plus
+                body!!.linearVelocity.add(Vector2(  speed, 0f))//to do: implement - for left and + for right
+            }                                   // ^ here
+            else (run == true)
+            {
 
+            }
+        }
     }
     fun die()
     {
@@ -106,9 +107,18 @@ class entity() {
     {
         if(isJumping)
         {
-            body.
+
+            if(jumpDuration> 0.toUInt()) {
+                body!!.applyForce(Vector2(0f, jumpPower / 10f), body!!.worldCenter, true)
+                bodyDefinition.linearVelocity.set(bodyDefinition.linearVelocity.x, bodyDefinition.linearVelocity.y + 0.5f)
+                jumpDuration--
+            }
+
+
+            else
+                isJumping=false
+                jumpDuration = jumpMaxDuration
         }
     }
     //Getters
-
 }
--- core/src/main/kotlin/CorePlat/main.kt
+++ core/src/main/kotlin/CorePlat/main.kt
@@ -9,8 +9,6 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
-import com.badlogic.gdx.utils.viewport.Viewport
-import javax.swing.text.html.parser.Entity
 
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
@@ -22,16 +20,37 @@ class main : ApplicationAdapter() {
     private var world: World? = null
     private val entities = mutableListOf<entity>()
 
-    fun createObject(createThis: entity) // will create reference or copy issues probably...
+    fun createObject(createThis: entity) : entity // will create reference or copy issues probably...
     {
         if(!entities.contains(createThis))
                 entities.add(createThis)
-        createThis.bodyInTheWorld = world!!.createBody(createThis.body) // i cant fix this rn, enough coding for today
+        createThis.body = world!!.createBody(createThis.bodyDefinition) // i cant fix this rn, enough coding for today
         var temporaryShape = PolygonShape()
         temporaryShape.setAsBox(50f, 1f)
-        createThis.bodyInTheWorld.createFixture(temporaryShape, 1f)
-
-        createThis.bodyInTheWorld.createFixture(temporaryShape, 1f )
+        createThis.body!!.createFixture(temporaryShape, 1f)
+        temporaryShape.dispose()
+        return createThis
+    }
+    fun createFloor()
+    {
+        val bodyDef = BodyDef()
+        bodyDef.type = BodyDef.BodyType.StaticBody
+        bodyDef.position[0f] = -10f
+        // add it to the world
+        // add it to the world
+        var bodys = world!!.createBody(bodyDef)
+        // set the shape (here we use a box 50 meters wide, 1 meter tall )
+        // set the shape (here we use a box 50 meters wide, 1 meter tall )
+        val shape = PolygonShape()
+        shape.setAsBox(50f, 1f)
+        // create the physical object in our bodyDefinition)
+        // without this our bodyDefinition would just be data in the world
+        // create the physical object in our bodyDefinition)
+        // without this our bodyDefinition would just be data in the world
+        bodys.createFixture(shape, 0.0f)
+        // we no longer use the shape object here so dispose of it.
+        // we no longer use the shape object here so dispose of it.
+        shape.dispose()
     }
     fun deleteObject(deleteThis: entity)
     {
@@ -39,21 +58,22 @@ class main : ApplicationAdapter() {
         if(entities.contains(deleteThis))
         {
             entities.remove(deleteThis)
-            world!!.destroyBody(deleteThis.bodyInTheWorld)
+            world!!.destroyBody(deleteThis.body)
         }
     }
 
     override fun create() {
         batch = SpriteBatch()
         image = Texture("badlogic.png")
-        player = entity()
         camera = OrthographicCamera()
         world = World(Vector2(0f,0f), true)
-
+        createFloor()
+        player = createObject(entity(Vector2(0f,0f), 100f, 10))
     }
     override fun render() {
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        applyLogic()
         batch!!.begin()
         batch!!.draw(image, 165f, 180f)
         batch!!.end()
@@ -67,6 +87,11 @@ class main : ApplicationAdapter() {
     fun applyLogic()
     {
         world!!.step(Gdx.graphics.deltaTime, 3,3 ) //no idea what those last2 args mean
+        for(element in entities)
+        {
+            element.body!!.applyForceToCenter(world!!.gravity,true)
+            element.update()
+        }
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
         {
 
